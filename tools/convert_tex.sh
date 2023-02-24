4975e5f68d47cdda1d23cf6592013e5651aaea67
sigh <sigh.projects@gmail.com>
2023-02-25 09:52:34 +1100

    Add a conversion script.

---
+++ tools/convert_tex.sh
@@ -0,0 +1,13 @@
+sed -E -i '' 's/\\section{([^}]*)}/# \1/g' "$1"
+sed -E -i '' 's/\\begin{solution}/{\::nomarkdown}\
+<label for="{{ include.id }}"><\/label>\
+<input type="checkbox" class="solution" id="{{ include.id }}">\
+{:\/}\
+<div>/g' "$1"
+sed -E -i '' 's/\\end{solution}/<\/div>/g' "$1"
+sed -E -i '' 's/\\end{([^}]*)}//g' "$1"
+sed -E -i '' 's/\\begin{([^}]*)}//g' "$1"
+sed -E -i '' 's/\\newline//g' "$1"
+sed -E -i '' 's/\\subsubsection\*{([^}]*)}/### \1/g' "$1"
+sed -E -i '' 's/\$(.)\$/$$ \1 $$/g' "$1"
+sed -E -i '' 's/\\item/*/g' "$1"
